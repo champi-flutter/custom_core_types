2e82952338257a30b1c605e46386463b89505558
式田勇樹 <y.shikita@RV232-3.local>
2026-06-05 18:02:30 +0900

    custom_core_types 初push

---
+++ android/app/src/main/kotlin/com/appchampignon/custom_core_types/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.appchampignon.custom_core_types
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/custom_core_types.dart
@@ -0,0 +1,7 @@
+library custom_core_types;
+
+// custom_core_types
+export 'package:custom_core_types/custom_core_types/result.dart';
+
+// extended_core
+export 'package:custom_core_types/extended_core/date_time_options.dart';
