be86a6d889eac487f5a25577ac547505f3c1bf35
式田勇樹 <y.shikita@RV232-3.local>
2026-06-30 16:04:58 +0900

    non_specified 追加

--- lib/custom_core_types.dart
+++ lib/custom_core_types.dart
@@ -2,6 +2,8 @@ library custom_core_types;
 
 // custom_core_types
 export 'package:custom_core_types/custom_core_types/result.dart';
+// 2026/06/30 追加
+export 'package:custom_core_types/custom_core_types/non_specified.dart';
 // 2026/06/08 追加: /custom_date_type
 export 'package:custom_core_types/custom_core_types/custom_date_type/date.dart';
 
---
+++ lib/custom_core_types/non_specified.dart
@@ -0,0 +1,2 @@
+/// 引数で指定されていないことを識別する値
+const Object nonSpecified = Object();
