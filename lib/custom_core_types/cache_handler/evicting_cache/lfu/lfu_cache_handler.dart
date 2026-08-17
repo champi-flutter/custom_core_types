c8d5b6e66d19ab3470341c98d74710cb15dd574c
式田勇樹 <y.shikita@RV232-3.local>
2026-08-17 16:12:21 +0900

    LfuCacheHandler にコメント追加

--- lib/custom_core_types/cache_handler/evicting_cache/lfu/lfu_cache_handler.dart
+++ lib/custom_core_types/cache_handler/evicting_cache/lfu/lfu_cache_handler.dart
@@ -5,6 +5,18 @@ import 'package:custom_core_types/custom_core_types/cache_handler/evicting_cache
 
 /// LFU のキャッシュハンドラ
 ///
+/// ジェネリクスに識別子とデータの型を指定する。
+///
+/// ```
+/// /// 識別子の型が `int` 、データの型が `SampleData` の場合
+/// abstract class SampleCacheHandler
+///     extends LfuCacheHandler<int, List<SampleData>> {
+///   /// `super.capacity` を親に渡すための内部的なコンストラクタ
+///   /// （抽象クラスなので呼び出し不可）
+///   SampleCacheHandler({required super.capacity});
+/// }
+/// ```
+///
 /// このクラスを継承して、[output] に、キャッシュ更新時の反映処理を記述する。
 abstract class LfuCacheHandler<K, V>
     extends BaseCacheHandler<K, V, LfuCache<K, V>> {
