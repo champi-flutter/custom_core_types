94eb1fa4b402d45e54a0ee459a5b6d0c38b7903a
式田勇樹 <y.shikita@RV232-3.local>
2026-08-27 20:58:58 +0900

    TwoDMapOptions 追加

--- lib/extended_core/map_options.dart
+++ lib/extended_core/map_options.dart
@@ -6,3 +6,11 @@ extension ListMapOptions<K, V> on Map<K, List<V>>{
     putIfAbsent(key, () => []).add(value);
   }
 }
+
+/// 二重の Map の処理
+extension TwoDMapOptions<K, SubK, V> on Map<K, Map<SubK, V>>{
+  /// Map の value が Map の時の代入プロセス
+  void addNullable({required K key, required SubK subKey, required V value}){
+    putIfAbsent(key, () => <SubK, V>{})[subKey] = value;
+  }
+}
