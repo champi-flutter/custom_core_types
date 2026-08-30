81e4b4d024e138e11df45e56e0df0c6196c4118b
式田勇樹 <y.shikita@RV232-3.local>
2026-08-30 17:35:56 +0900

    BaseFloatingListCacheHandler 修正

--- lib/custom_core_types/cache_handler/list_cache_handler/floating_list_cache/base_floating_list_cache/base_floating_list_cache_handler.dart
+++ lib/custom_core_types/cache_handler/list_cache_handler/floating_list_cache/base_floating_list_cache/base_floating_list_cache_handler.dart
@@ -9,8 +9,8 @@ import 'package:flutter/foundation.dart';
 /// [BaseListCacheHandler] に [BaseFloatingListCache] の機能を付与したハンドラの
 /// 基底クラス
 ///
-/// 付与機能には、[BaseFloatingListCacheHandler.cacheMap] からアクセスする。
-///  - [cacheMap.moveTo]: 指定ID（リストで複数選択可）を、指定の key（第1引数）に移動する
+/// 付与機能
+///  - [moveTo]: 指定ID（リストで複数選択可）を、指定の key（第1引数）に移動する
 ///
 abstract class BaseFloatingListCacheHandler<
   K,
@@ -19,6 +19,13 @@ abstract class BaseFloatingListCacheHandler<
   Ent extends BaseCacheEntry<V>,
   C extends BaseCache<I, V, Ent>,
   M extends BaseFloatingListCache<K, I, V, Ent, C>
-> extends BaseListCacheHandler<K, I, V, Ent, C, M>{
+>
+    extends BaseListCacheHandler<K, I, V, Ent, C, M> {
   BaseFloatingListCacheHandler(super.cacheMap);
+
+  /// ID で指定したエントリを、[key] に移動する
+  ///
+  /// fixme 計算量多め（最大 O(n^2） <= 数個の ID ならほぼ O(n)）
+  void moveTo(K key, {required List<I> orderedId}) =>
+      cacheMap.moveTo(key, orderedId: orderedId);
 }
