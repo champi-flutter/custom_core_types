b653ba850f304dc2704f68811413fd8cb7cfa48e
式田勇樹 <y.shikita@RV232-3.local>
2026-09-12 14:31:20 +0900

    FixedList.mapValue を追加

--- lib/custom_core_types/custom_collection/fixed_list.dart
+++ lib/custom_core_types/custom_collection/fixed_list.dart
@@ -13,13 +13,18 @@ import 'dart:collection';
 abstract class FixedList<E> extends ListBase<ListEntry<E>> {
   final List<ListEntry<E>> _list;
 
-  /// サブクラスから「要素数(length)」と「初期値生成処理(fill)」を受け取る
   FixedList(int length, E Function(int index) fill)
       : _list = List.generate(
     length,
         (i) => ListEntry._(i, fill(i)),
     growable: false, // 固定長化
   );
+
+  /// Iterable から FixedList を生成するファクトリコンストラクタ
+  factory FixedList._fromIterable(Iterable<E> iterable) {
+    final list = iterable.toList(growable: false);
+    return _FixedListFromList._(list);
+  }
 
   @override
   int get length => _list.length;
@@ -43,11 +48,28 @@ abstract class FixedList<E> extends ListBase<ListEntry<E>> {
   void operator []=(int index, ListEntry<E> newEntry) {
     _list[index] = newEntry;
   }
+
+  /// 各要素を [convert] に従って変換し、その戻り値の型の [Iterable] を返す
+  Iterable<R> mapValues<R>(R Function(E value) convert) {
+    return _list.map((entry) => convert(entry.value));
+  }
 }
 
+/// [FixedList] のインデックスと要素の組み合わせ
 class ListEntry<E> {
   final int index;
   E value;
 
   ListEntry._(this.index, this.value);
+}
+
+/// 変換メソッドのためのプライベートクラス
+class _FixedListFromList<E> extends FixedList<E> {
+  _FixedListFromList._(List<E> list)
+      : super(list.length, (i) => list[i]);
+}
+
+extension ToFixedList<E> on Iterable<E> {
+  /// [Iterable] を [FixedList] に変換する
+  FixedList<E> toFixedList() => FixedList._fromIterable(this);
 }
